471e69976d946477a5be2fe44dc75ce7631dbf91
Kurniakun17 <kurniakharisma17@gmail.com>
2024-04-27 13:43:22 +0700

    Initial Commit

---
+++ Multipeer/MultipeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipeerApp.swift
+//  Multipeer
+//
+//  Created by Kurnia Kharisma Agung Samiadjie on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MultipeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
